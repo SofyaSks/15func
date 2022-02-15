83fc11891edb1523659497683a788f44d778ad7b
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2022-02-15 21:14:06 +0500

    ещё изменения

--- 15func/15func.cpp
+++ 15func/15func.cpp
@@ -15,17 +15,30 @@ double func(double num1, double num2) {
 template <typename T1, typename T2> T1 maxfunc(T1 num1, T2 num2) {
 	return num1 > num2 ? num1 : num2;
 }
+template <typename T> T mean(T array[], int length) {
+	T sum = 0;
+	for (int i = 0; i < length; i++)
+		sum += array[i];
+	return sum / length;
+}
 
 
 int main() {
 	setlocale(LC_ALL, "ru");
 	
-	float n, m;
+	/*float n, m;
 	cin >> n >> m;
-	cout << maxfunc(n, m) << endl;
+	cout << maxfunc(n, m) << endl;*/
+
+	// Задача 1
+	/*cout << "Изначальный массив: ";
+	float z1[5]{ 8.5, 1.6, 2.5, 3.5, 2.4 };
+	for (int i = 0; i < 5; i++)
+		cout << z1[i] << " ";
+	cout << "\nСреднее арифметическое элементов массива = " << mean(z1,5)<< endl;*/
 
 
-	cout << "Finish";
+
 
 
 
