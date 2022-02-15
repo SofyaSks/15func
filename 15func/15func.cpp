3f2331e63588a169bd6be8d8bfd059425e03ec3c
Ж - 6 < - 6@DESKTOP-FS4GDJL>
2022-02-15 20:39:07 +0500

    Add project files.

---
+++ 15func/15func.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+using namespace std;
+
+/*int func(int num1, int num2) {
+	cout << "Целые числа\n";
+	return (num1 > num2 ? num1: num2);
+}
+double func(double num1, double num2) {
+	cout << "Дробные числа\n";
+	return num1 > num2 ? num1 : num2;
+}*/
+
+
+template <typename T1, typename T2> T1 maxfunc(T1 num1, T2 num2) {
+	return num1 > num2 ? num1 : num2;
+}
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	
+	float n, m;
+	cin >> n >> m;
+	cout << maxfunc(n, m) << endl;
+
+
+
+
+
+
+
+
+
+
+	/*int n, m;
+	cin >> n >> m;
+	cout << func(n,m) << endl;
+	cout << func(5.7, 9.1) << endl;*/
+	
+
+	return 0;
+}
